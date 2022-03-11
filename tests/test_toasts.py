18f14b306d0d731561ce95ef014efc87012fe009
DatGuy <datguysteam@gmail.com>
2022-03-11 13:40:11 +0200

    Try fixing test fails on platforms that don't support notifications
    GitHub for example throws 'OSError: [WinError -2143420155] The notification platform is unavailable.' Prefer this to manually excluding tests in case the tests are run on other platforms that don't support toast notifications
    
    Used wrong error number, fixing
    
    Change errno to WinError

--- tests/test_toasts.py
+++ tests/test_toasts.py
@@ -1,31 +1,45 @@
 def test_simple_toast():
-    from windows_toasts import ToastText1, WindowsToaster
-    winToaster = WindowsToaster("Python")
+    try:
+        from windows_toasts import ToastText1, WindowsToaster
+        winToaster = WindowsToaster("Python")
 
-    simpleToast = ToastText1()
-    simpleToast.SetBody("Hello, simple world!")
-    simpleToast.on_activated = lambda _: print("Toast clicked!")
+        simpleToast = ToastText1()
+        simpleToast.SetBody("Hello, simple world!")
+        simpleToast.on_activated = lambda _: print("Toast clicked!")
 
-    winToaster.show_toast(simpleToast)
+        winToaster.show_toast(simpleToast)
+    except OSError as osError:
+        if osError.winerror == -2143420155:
+            print("The testing platform doesn't support notifications. Skipping test_simple_toast")
+            return
+
+        raise
 
 
 def test_interactable_toast():
-    from windows_toasts import (
-        AudioSource, InteractableWindowsToaster, ToastActivatedEventArgs, ToastAudio, ToastImageAndText2
-    )
+    try:
+        from windows_toasts import (
+            AudioSource, InteractableWindowsToaster, ToastActivatedEventArgs, ToastAudio, ToastImageAndText2
+        )
 
-    def notificationActivated(activatedEventArgs: ToastActivatedEventArgs):
-        print(f"Clicked event args: {activatedEventArgs.arguments}")
-        print(activatedEventArgs.input)
+        def notificationActivated(activatedEventArgs: ToastActivatedEventArgs):
+            print(f"Clicked event args: {activatedEventArgs.arguments}")
+            print(activatedEventArgs.input)
 
-    winToaster = InteractableWindowsToaster("Python")
+        winToaster = InteractableWindowsToaster("Python")
 
-    newToast = ToastImageAndText2()
-    newToast.SetBody("Hello, interactable world!")
-    newToast.AddAction("First", "clicked=first")
-    newToast.AddAction("Second", "clicked=second")
-    newToast.SetInputField("Write your placeholder text here!")
-    newToast.audio = ToastAudio(AudioSource.IM, looping=True)
+        newToast = ToastImageAndText2()
+        newToast.SetBody("Hello, interactable world!")
+        newToast.AddAction("First", "clicked=first")
+        newToast.AddAction("Second", "clicked=second")
+        newToast.SetInputField("Write your placeholder text here!")
+        newToast.audio = ToastAudio(AudioSource.IM, looping=True)
 
-    newToast.on_activated = notificationActivated
-    winToaster.show_toast(newToast)
+        newToast.on_activated = notificationActivated
+        winToaster.show_toast(newToast)
+    except OSError as osError:
+        if osError.winerror == -2143420155:
+            print("The testing platform doesn't support notifications. Skipping test_simple_toast")
+            return
+
+        raise
