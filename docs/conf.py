b95b4e25e6244d0c1ff0ee713cf6b69eec42d8ad
DatGuy <datguysteam@gmail.com>
2023-03-12 13:24:41 +0200

    Remove unused imports

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
-import sys
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
