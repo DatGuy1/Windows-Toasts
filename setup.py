4b112afb8f4b445413a7ffe439b6e21fc24ee49b
DatGuy <datguysteam@gmail.com>
2023-03-24 18:37:44 +0300

    Move to using _version instead of hardcoding
    Reduces duplication

--- setup.py
+++ setup.py
@@ -7,15 +7,19 @@ requires = ["winsdk"]
 with open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
 
+about = {}
+with open("src/windows_toasts/_version.py", "r") as f:
+    exec(f.read(), None, about)
+
 setup(
-    name="windows-toasts",
-    version="0.4.0",
-    description="Windows toast notifications sender",
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
     long_description=readme,
     long_description_content_type="text/markdown",
-    author="DatGuy",
+    author=about["__author__"],
     author_email="datguysteam@gmail.com",
-    url="https://github.com/DatGuy1/Windows-Toasts",
+    url=about["__url__"],
     packages=packages,
     package_dir={"windows_toasts": "src/windows_toasts"},
     package_data={"": ["LICENSE"], "windows_toasts": ["py.typed"]},
@@ -23,7 +27,7 @@ setup(
     entry_points={"console_scripts": ["register_hkey_aumid = scripts.register_hkey_aumid:main"]},
     python_requires=">=3.8",
     install_requires=requires,
-    license="Apache 2.0",
+    license=about["__license__"],
     zip_safe=False,
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -38,7 +42,7 @@ setup(
         "Operating System :: Microsoft :: Windows :: Windows 11",
     ],
     project_urls={
-        "Bug Tracker": "https://github.com/DatGuy1/Windows-Toasts/issues",
         "Documentation": "https://windows-toasts.readthedocs.io",
+        "Source": "https://github.com/DatGuy1/Windows-Toasts",
     },
 )
