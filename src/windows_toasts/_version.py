0692a381f181652c409f93d6f984560eeb82285b
DatGuy <datguysteam@gmail.com>
2023-08-14 23:34:56 +0300

    release: release v1.0.0 🎉

--- src/windows_toasts/_version.py
+++ src/windows_toasts/_version.py
@@ -1,6 +1,6 @@
 __title__ = "Windows-Toasts"
 __description__ = "Python library used to send toast notifications on Windows machines"
 __url__ = "https://github.com/DatGuy1/Windows-Toasts"
-__version__ = "0.4.2.dev1"
+__version__ = "1.0.0"
 __author__ = "DatGuy"
 __license__ = "Apache 2.0"
