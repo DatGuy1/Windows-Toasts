d032de9b2b298e86f27022d65c3f1de984879003
DatGuy1 <datguysteam@gmail.com>
2024-06-30 17:31:56 +0300

    chore: soften winrt packages version specification (#153)
    Should now support version 2.0 up to 3.0, excluding. Resolves #152

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 packages = ["windows_toasts", "scripts"]
 
 requires = [
-    "winrt-runtime<=2.0.1",
-    "winrt-Windows.Data.Xml.Dom<=2.0.1",
-    "winrt-Windows.Foundation<=2.0.1",
-    "winrt-Windows.Foundation.Collections<=2.0.1",
-    "winrt-Windows.UI.Notifications<=2.0.1",
+    "winrt-runtime~=2.0",
+    "winrt-Windows.Data.Xml.Dom~=2.0",
+    "winrt-Windows.Foundation~=2.0",
+    "winrt-Windows.Foundation.Collections~=2.0",
+    "winrt-Windows.UI.Notifications~=2.0",
 ]
 
 with open("README.md", "r", encoding="utf-8") as f:
