DatGuy1/Windows-Toasts

commit a4a998547fb1b524eb02f4787ff38ad6be3fa790

feat: support removing toasts and toast groups (#145)
* feat: support removing toasts and toast groups

* tests(remove): add tests for removing toasts

* docs(remove): provide toast removal example

* tests(remove): remove the toasts unconditionally

Don't wait for on_activated or on_dismissed, otherwise the coverage doesn't run

* style(remove): sort imports in test

[skip ci]

* docs(changelog): add entry for toast removal

[skip ci]

* docs(changelog): correct indentation

[skip ci]

src/windows_toasts/toasters.py
@@ -64,8 +64,8 @@ def _build_toast_notification(toast: Toast, toastNotification: ToastNotification
     :rtype: ToastNotificationT
     """
     toastNotification.tag = toast.tag
-    if toast.group is not None:
-        toastNotification.group = toast.group
+    # Group, a non-empty string, is required for some functionality. If one isn't provided, use the tag
+    toastNotification.group = toast.group or toast.tag
 
     if toast.expiration_time is not None:
         toastNotification.expiration_time = toast.expiration_time
@@ -207,6 +207,21 @@ class BaseWindowsToaster:
         scheduledToasts = self.toastNotifier.get_scheduled_toast_notifications()
         for toast in scheduledToasts:
             self.toastNotifier.remove_from_schedule(toast)
+
+    def remove_toast(self, toast: Toast) -> None:
+        """
+        Removes an individual popped toast
+        """
+        # Is fetching toastHistory expensive? Should this be stored in an instance variable?
+        toastHistory: ToastNotificationHistory = ToastNotificationManager.history
+        toastHistory.remove(toast.tag, toast.group or toast.tag, self._AUMID)
+
+    def remove_toast_group(self, toastGroup: str) -> None:
+        """
+        Removes a group of toast notifications, identified by the specified group ID
+        """
+        toastHistory: ToastNotificationHistory = ToastNotificationManager.history
+        toastHistory.remove_group(toastGroup, self._AUMID)
 
 
 class WindowsToaster(BaseWindowsToaster):

src/windows_toasts/toast.py
@@ -34,7 +34,7 @@ class Toast:
     expiration_time: Optional[datetime.datetime]
     """The time for the toast to expire on in the action center. If it is on-screen, nothing will happen"""
     group: Optional[str]
-    """A generic identifier, where you can assign groups like "wallPosts", "messages", "friendRequests", etc."""
+    """An internal identifier, where you can assign groups like "wallPosts", "messages", "friendRequests", etc."""
     scenario: ToastScenario
     """Scenario for the toast"""
     suppress_popup: bool
@@ -60,7 +60,7 @@ class Toast:
     text_fields: list[Optional[str]]
     """Various text fields"""
     tag: str
-    """uuid of a tag for the toast"""
+    """Unique tag for the toast, automatically set as a UUID"""
     updates: int
     """Number of times the toast has been updated; mostly for internal use"""
     _launch_action: Optional[str]

tests/conftest.py
@@ -13,7 +13,7 @@ def real_run_fixture(pytestconfig) -> Iterator[None]:
     else:
         with patch("winrt.windows.ui.notifications.ToastNotificationManager.create_toast_notifier"), patch(
             "winrt.windows.ui.notifications.ToastNotifier.show"
-        ), patch("winrt.windows.ui.notifications.ToastNotificationHistory.clear"):
+        ), patch("winrt.windows.ui.notifications.ToastNotificationHistory.clear"), patch("time.sleep"):
             yield
 
 

tests/test_toasts.py
@@ -296,3 +296,37 @@ def test_system_toast():
     newToast.AddAction(dismissBox)
 
     InteractableWindowsToaster("Python").show_toast(newToast)
+
+
+def test_remove_toast():
+    toaster = WindowsToaster("Python")
+    newToast = Toast(["Disappearing act"])
+
+    toaster.show_toast(newToast)
+
+    import time
+
+    time.sleep(0.5)
+
+    toaster.remove_toast(newToast)
+
+
+def test_remove_toast_group():
+    import time
+
+    from src.windows_toasts import ToastActivatedEventArgs
+
+    toaster = WindowsToaster("Python")
+
+    toast1 = Toast(["A bigger disappearing act"], group="begone")
+    toast2 = Toast(["Click me to shazam!"], group="begone")
+
+    toast2.on_activated = lambda _: toaster.remove_toast_group("begone")
+
+    toaster.show_toast(toast1)
+    toaster.show_toast(toast2)
+
+    # If it hasn't been activated, remove it ourselves
+    time.sleep(1)
+
+    toast2.on_activated(ToastActivatedEventArgs())

commit 85b29663c43878bc7d4f8af58699833ca8460a1d

fix(reg): include group when updating toast
Regression introduced in a4a998547fb1b524eb02f4787ff38ad6be3fa790
Resolves #163

src/windows_toasts/toasters.py
@@ -162,7 +162,7 @@ class BaseWindowsToaster:
         :return: Whether the update succeeded
         """
         newData = _build_adaptable_data(toast)
-        return self.toastNotifier.update(newData, toast.tag) == NotificationUpdateResult.SUCCEEDED
+        return self.toastNotifier.update(newData, toast.tag, toast.group or toast.tag) == NotificationUpdateResult.SUCCEEDED
 
     def schedule_toast(self, toast: Toast, displayTime: datetime) -> None:
         """